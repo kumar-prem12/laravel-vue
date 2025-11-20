f3683bbcfd18c9482cf4d0a1059f7a1417e89a4b
prem <prem72965@gmail.com>
2025-11-20 22:37:30 +0530

    route chages

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,20 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// Route::prefix('dashboard')->middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/', function () {
+//         return Inertia::render('Dashboard');
+//     })->name('dashboard');
+// });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 require __DIR__.'/settings.php';
